e3dddffaa57d7927f509a10ebd3e96cd2c7af396
ThariusIron11 <eternalflame357@gmail.com>
2026-08-31 19:36:12 -0500

    Delete envios.js

--- envios.js
+++
@@ -1,7 +0,0 @@
-// ================== Pestaña: Envíos / Despachos ==================
-// Lógica de armado de envíos, remesas, remisiones y despacho.
-
-document.addEventListener('tab:activada', (e) => {
-  if (e.detail.tab !== 'envios') return;
-  // TODO: inicializar/refrescar la pestaña Envíos al entrar
-});
