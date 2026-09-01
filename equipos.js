413e8deaa9fcfa007aabf5800a5b28566e92e160
ThariusIron11 <eternalflame357@gmail.com>
2026-08-31 19:36:41 -0500

    Delete equipos.js

--- equipos.js
+++
@@ -1,7 +0,0 @@
-// ================== Pestaña: Equipos ==================
-// Lógica del catálogo/selección de equipos que entran en un pedido.
-
-document.addEventListener('tab:activada', (e) => {
-  if (e.detail.tab !== 'equipos') return;
-  // TODO: inicializar/refrescar la pestaña Equipos al entrar
-});
